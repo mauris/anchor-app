4145d62a2c1fda529bac31667411a0c59890e5cf
Sam-Mauris Yong <mauris@hotmail.sg>
2012-11-04 21:41:40 +0800

    config writer implementation

---
+++ anchor/ConfigWriter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace anchor
+{
+    class ConfigWriter
+    {
+        private string path;
+
+        public ConfigWriter(string path)
+        {
+            this.path = path;
+        }
+
+        public void write(string root, List<Entry> entries)
+        {
+            FileStream file = File.Open(this.path, FileMode.Create);
+            StreamWriter writer = new StreamWriter(file);
+            writer.WriteLine("NameVirtualHost 127.0.0.1");
+            writer.WriteLine("<VirtualHost 127.0.0.1>");
+            writer.WriteLine("    ServerName localhost:80");
+            writer.WriteLine("    DocumentRoot '" + root + "'");
+            writer.WriteLine("</VirtualHost>");
+            foreach(Entry entry in entries)
+            {
+                writer.WriteLine("<VirtualHost 127.0.0.1>");
+                writer.WriteLine("    ServerName " + entry.Name + ".dev:80");
+                writer.WriteLine("    DocumentRoot '" + entry.Path + "'");
+                writer.WriteLine("</VirtualHost>");
+                writer.WriteLine("<Directory \"" + entry.Path + "\">");
+                writer.WriteLine("    Allow from all");
+                writer.WriteLine("    Order Deny,Allow");
+                writer.WriteLine("</VirtualHost>");
+            }
+        }
+
+    }
+}
