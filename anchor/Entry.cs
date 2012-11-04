c37dbd053e205134855581d4ab3b57282f894578
Sam-Mauris Yong <mauris@hotmail.sg>
2012-11-04 20:14:27 +0800

    Entry added

---
+++ anchor/Entry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace anchor
+{
+    class Entry
+    {
+
+        public string Name{get; set;}
+
+        public string Path { get; set; }
+
+    }
+}
