d449e510ccc04f28676d64d9e18e830e0d71a496
Sam-Mauris Yong <mauris@hotmail.sg>
2012-11-05 11:29:53 +0800

    settings inclusion

---
+++ anchor/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace anchor
+{
+    class Settings
+    {
+
+        public string WampServerPath { get; set; }
+
+    }
+}
