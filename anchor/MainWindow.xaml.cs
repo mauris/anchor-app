dbdfa8308329a202d5c18ae7a74ba27c6dbce792
Sam-Mauris Yong <mauris@hotmail.sg>
2012-11-05 23:02:31 +0800

    delegated styling of toolbar and content shadow to xaml

--- anchor/MainWindow.xaml.cs
+++ anchor/MainWindow.xaml.cs
@@ -26,8 +26,6 @@ namespace anchor
         {
             InitializeComponent();
 
-            pnlToolbar.Background = new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "Assets/toolbar-background.png")));
-            pnlContent.Background = new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "Assets/toolbar-shadow.png"))) { Stretch = Stretch.None, AlignmentY = AlignmentY.Top, AlignmentX = AlignmentX.Left, Viewport = new Rect(0, 0, 20, 1000000), ViewportUnits = BrushMappingMode.Absolute, TileMode = TileMode.FlipXY };
         }
 
         ~MainWindow()
