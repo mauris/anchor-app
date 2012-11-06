8b55c4426a6ee92f1ac901cd3231786b505d0457
Sam-Mauris Yong <mauris@hotmail.sg>
2012-11-06 19:41:01 +0800

    open browser link implementation

--- anchor/MainWindow.xaml.cs
+++ anchor/MainWindow.xaml.cs
@@ -110,7 +110,8 @@ namespace anchor
 
         private void btnHostOpen_Click(object sender, RoutedEventArgs e)
         {
-
+            Entry entry = (Entry)lstSites.SelectedItem;
+            Process.Start("http://" + entry.Name + ".dev/");
         }
 
         private void btnHostBrowse_Click(object sender, RoutedEventArgs e)
