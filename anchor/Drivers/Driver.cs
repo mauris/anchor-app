99b4cb7f19d6ee3484c7d288e7c9144eda1a5752
Sam-Mauris Yong <mauris@hotmail.sg>
2012-11-06 20:22:42 +0800

    driver implementation and ini parser inclusion

---
+++ anchor/Drivers/Driver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace anchor.Drivers
+{
+    abstract class Driver
+    {
+        public string ApacheRestartCommand { get; set; }
+
+        public string ApacheConfigPath { get; set; }
+    }
+}
---
+++ anchor/Drivers/WampServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using IniParser;
+
+namespace anchor.Drivers
+{
+    class WampServer : Driver
+    {
+        public WampServer(string path)
+        {
+            FileIniDataParser parser = new FileIniDataParser();
+            IniData data = parser.LoadFile(Path.Combine(path, "wampmanager.conf"));
+            string version = data["apache"]["apacheVersion"];
+            version = version.Trim('"', '\'');
+            this.ApacheRestartCommand = Path.Combine(path, "bin\\apache\\apache" + version + "\\bin\\httpd.exe") + " -k graceful";
+            this.ApacheConfigPath = Path.Combine(path, "conf\\");
+        }
+    }
+}
