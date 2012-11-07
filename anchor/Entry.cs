069532abd7d8d0cf6ddddc87ac99fe60eda0a4cc
Sam-Mauris Yong <mauris@hotmail.sg>
2012-11-07 16:21:45 +0800

    favicon updates

--- anchor/Entry.cs
+++ anchor/Entry.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace anchor
 {
@@ -9,9 +14,34 @@ namespace anchor
     class Entry
     {
 
+        [DllImport("gdi32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool DeleteObject(IntPtr value);
+
         public string Name{get; set;}
 
         public string Path { get; set; }
+
+        public ImageSource GetFavicon
+        {
+            get
+            {
+                var bitmap = Favicon.fetch(this.Path, this.Name + ".dev");
+                IntPtr bmpPt = bitmap.GetHbitmap();
+                BitmapSource bitmapSource =
+                 System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                       bmpPt,
+                       IntPtr.Zero,
+                       Int32Rect.Empty,
+                       BitmapSizeOptions.FromEmptyOptions());
+
+                //freeze bitmapSource and clear memory to avoid memory leaks
+                bitmapSource.Freeze();
+                DeleteObject(bmpPt);
+
+                return bitmapSource;
+            }
+        }
 
     }
 }
