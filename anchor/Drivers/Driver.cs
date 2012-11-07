a8ad50125188c6fcc777c1ea72458f0d0e961645
Sam-Mauris Yong <mauris@hotmail.sg>
2012-11-07 14:12:12 +0800

    driver updates

--- anchor/Drivers/Driver.cs
+++ anchor/Drivers/Driver.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace anchor.Drivers
 {
     abstract class Driver
     {
-        public string ApacheRestartCommand { get; set; }
-
         public string ApacheConfigPath { get; set; }
+
+        public string ServerRootPath { get; set; }
+
+        public abstract void restartServer();
     }
 }
--- anchor/Drivers/WampServer.cs
+++ anchor/Drivers/WampServer.cs
@@ -4,19 +4,32 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using IniParser;
+using System.Diagnostics;
 
 namespace anchor.Drivers
 {
     class WampServer : Driver
     {
+        private string rootPath;
+
+        private string apacheVersion;
+
         public WampServer(string path)
         {
+            rootPath = path;
             FileIniDataParser parser = new FileIniDataParser();
             IniData data = parser.LoadFile(Path.Combine(path, "wampmanager.conf"));
             string version = data["apache"]["apacheVersion"];
-            version = version.Trim('"', '\'');
-            this.ApacheRestartCommand = Path.Combine(path, "bin\\apache\\apache" + version + "\\bin\\httpd.exe") + " -k graceful";
-            this.ApacheConfigPath = Path.Combine(path, "conf\\");
+            apacheVersion = version.Trim('"', '\'');
+            this.ApacheConfigPath = Path.Combine(path, "alias\\");
+            this.ServerRootPath = Path.Combine(path, "www");
+        }
+
+        public override void restartServer()
+        {
+            ProcessStartInfo info = new ProcessStartInfo(Path.Combine(rootPath, "bin\\apache\\apache" + apacheVersion + "\\bin\\httpd.exe"), "-n wampapache -k restart");
+            info.CreateNoWindow = true;
+            Process.Start(info);
         }
     }
 }
