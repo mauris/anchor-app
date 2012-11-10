ee8f54a88ccd90462c41dc5a634fbf9c038566ae
Sam-Mauris Yong <mauris@hotmail.sg>
2012-11-10 23:28:00 +0800

    anchor.commons library project

---
+++ anchor.commons/DataFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace anchor.commons
+{
+    public static class DataFile
+    {
+
+        public static void Write(string path, object data)
+        {
+            using (Stream stream = File.Open(path, FileMode.Create))
+            {
+                BinaryFormatter bin = new BinaryFormatter();
+                bin.Serialize(stream, data);
+            }
+        }
+
+        public static object Read(string path)
+        {
+            object data = null;
+            using (Stream stream = File.OpenRead(path))
+            {
+                BinaryFormatter bin = new BinaryFormatter();
+                data = bin.Deserialize(stream);
+            }
+            return data;
+        }
+
+        public static T Read<T>(string path)
+        {
+            return (T)Read(path);
+        }
+
+    }
+}
